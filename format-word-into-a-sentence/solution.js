813a6d74f823aae1648d1b0ab38a1716933f390f
AQHawk <ironhawkcreations@gmail.com>
2017-11-02 09:55:09 -0700

    Format words into a sentence

---
+++ format-word-into-a-sentence/solution.js
@@ -0,0 +1,21 @@
+'use strict';
+function formatWords(words){
+if (!words) return '';
+
+ for (i=0; i<words.length; i++) {
+     if(words[i] === ''){
+     words.splice(i,1);
+      i--;
+    }
+  }
+
+ if (words.length>1){
+    let arr = [words.slice(0,-1).join(', '), words[words.length - 1]];
+  let str = arr.join(' and ');
+  return str;
+  }
+  else{
+  let str = words.join('');
+  return str;
+  }
+}
